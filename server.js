b69059fcdd69d11b4c1fcfa413b9421f6ac1f108
Paul Pinelo <paulth2311@gmail.com>
2018-04-10 14:27:38 -0500

    //

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const
     app = require('./app'),
     conf = require('./config/global');
 
-var port = conf.app.port,
+var port = conf.app.port;
     // hostname = conf.app.hostname;
 
 
-app.listen( port,
-            ()=>{
+app.listen( port,()=>{
             console.log("SERVIDOR CORRIENDO EN EL PUERTO " + port);
 });
