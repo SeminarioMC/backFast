d7794ad753db2bbc3f6a057fcd75c2bcbb8c8207
Paul Pinelo <paulth2311@gmail.com>
2018-04-27 20:58:18 -0500

    save

--- app/Inscripcion/inscripcion.controller.js
+++ app/Inscripcion/inscripcion.controller.js
@@ -6,7 +6,7 @@ module.exports.insert=function (req,res) {
     var newIns=ins(req.body);
     newIns.save(function(err){
         if(err)
-            res.status(403).send({error:'eres un guffy'});
+            res.status(403).send({error:'error exist'});
         res.status(200).send({
             res:'OK'
         })
